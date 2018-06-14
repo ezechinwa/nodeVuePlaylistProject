c741bce9a995fa5d4813fac31776259c3666153d
Kranda <chinwawilliams@gmail.com>
2018-06-14 03:20:35 +0100

    Route, Api request set up

---
+++ client/src/components/Register.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1>This is the registration section of the website</h1>
+    <p>Happy registration</p>
+
+    <form action="/register" method="post">
+          <input type="email" name="email" v-model="email" placeholder="Email Address" >
+       <input type="password" name="password" v-model="password" placeholder="Password Address" >
+       <button @click="register"> Register here</button>
+    </form>
+    
+
+
+       <p v-if="email"> This will be displayed {{ this.email }}</p>
+    
+  </div>
+</template>
+
+<script>
+import Authentication from '@/services/Authentication'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      email: '',
+      password: '' }
+  },
+  methods :{
+  async  register(e){ 
+      e.preventDefault();
+      const response = await Authentication.register({
+        "email" : this.email , 
+        "password" : this.password
+      });
+
+      console.log(response.data)
+    }
+  },
+  watch: { 
+         email (value)
+            { 
+              console.log('this email value has changed to ' + value)
+            }
+        }, 
+  mounted() {
+      setTimeout(()=>{
+         this.email = "";
+      },3000)
+  }
+}
+</script>
